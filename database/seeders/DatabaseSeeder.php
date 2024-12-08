d5cde1e2ab0221beef31ac3576bc638f86c64721
Rivaldi Syaputra <rivaldi191@gmail.com>
2024-12-08 18:34:48 +0700

    Add data seeder role, email verified and category

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
 use App\Models\User;
+use Illuminate\Auth\Events\Verified;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
@@ -15,11 +17,13 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $roles = ['admin'];
+        // Add Data Role
+        $roles = ['admin', 'writer', 'editor', 'user'];
         foreach ($roles as $role) {
             Role::firstOrCreate(['name' => $role, 'guard_name' => 'web']);
         }
 
+        // Add User with Role
         $admin = User::firstOrCreate([
             'email' => 'admin@role.id',
         ], [
@@ -28,5 +32,24 @@ class DatabaseSeeder extends Seeder
         ]);
 
         $admin->assignRole('admin');
+
+        // Email Verified
+        if (!$admin->hasVerifiedEmail()) {
+            $admin->markEmailAsVerified();
+            event(new Verified($admin));
+        }
+
+        // Add Data Category
+        $categories = [
+            'game',
+            'review',
+            'tutorial',
+            'gadget'
+        ];
+        foreach ($categories as $categoryName) {
+            Category::create([
+                'name' => $categoryName
+            ]);
+        }
     }
 }
